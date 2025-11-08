434229b8abe73fb45cde3082226349183b2a788c
Barmatov Alexey <82975661+hellpes666@users.noreply.github.com>
2025-11-08 11:52:53 +0300

    upd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import {
-    AccountModule,
+	AccountModule,
 	ActionsModule,
 	AuthModule,
 	IntegrationsModule,
 	UserModule,
 } from './domains';
 import { PrismaModule } from './lib';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [
-        PrismaModule,
-        
-		    ConfigModule.forRoot({
+	imports: [
+		PrismaModule,
+		ConfigModule.forRoot({
 			envFilePath: ['.env'],
 			isGlobal: true,
 		}),
-
-		    UserModule,
-		    IntegrationsModule,
-		    ActionsModule,
-		    AccountModule,
+		UserModule,
+		IntegrationsModule,
+		ActionsModule,
+		AccountModule,
 		AuthModule,
 	],
 	controllers: [],
